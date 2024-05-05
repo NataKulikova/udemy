5acae02f200f674a49086d6f50b6e448b690cd93
Nata <example@gmail.com>
2024-05-05 13:32:20 +0300

    second commit

---
+++ sand.js
@@ -0,0 +1,17 @@
+const personalMovieDB = {
+    movies: []
+    };
+    
+const a = 1
+const b = 2
+const c = 'fnjrk'
+const d = 2
+
+
+personalMovieDB.movies[0] = b; //заменяем первый элемент на b
+personalMovieDB.movies[1] = c; //
+personalMovieDB.movies[d] = 'i';
+
+
+console.log(personalMovieDB);
+//console.log(personalMovieDB.movies[4]);
